067f08674681dde5d9198b09fa0aab3a03631664
gokoro <lincroe@gmail.com>
2024-01-03 05:41:11 +0900

    chore: change stylex plugin for css reset compatiblity

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import styleX from 'vite-plugin-stylex'
 import { stylexPlugin } from 'vite-plugin-stylex-dev'
 
 // https://vitejs.dev/config/
-export default defineConfig((config) => {
-  const { mode } = config
-
-  return {
-    plugins: [react(), mode === 'development' ? stylexPlugin() : styleX()],
-  }
+export default defineConfig({
+  plugins: [react(), stylexPlugin({ useCSSLayers: false })],
 })
